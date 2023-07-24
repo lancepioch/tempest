ef126c76bc0e055e90e0e55476898f46e65a6996
Lance Pioch <git@lance.sh>
2023-07-23 21:51:39 -0400

    Make this one test

--- tests/Feature/ProjectsTest.php
+++ tests/Feature/ProjectsTest.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Models\Project;
+use App\Models\Team;
 use App\Models\User;
 use App\Livewire\CreateProject;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -26,22 +27,35 @@ it('can save a project', function () {
     assertDatabaseHas('projects', $attributes);
 })->todo();
 
-it('can access a project that they own', function () {
-    $user = User::factory()->create();
-    $user->projects()->save($project = Project::factory()->create());
+it('can only be viewed by team members that are apart of the project', function () {
+    // Create a team with a team leader
+    $teamLeader = User::factory()->create();
+    $team = Team::factory()->create(['user_id' => $teamLeader->id]);
 
-    Livewire::actingAs($user)
-        ->test(ViewProject::class)
+    // Add team members
+    $teamMember = User::factory()->create();
+    $team->users()->attach($teamMember);
+
+    // Create a project assigned to the team
+    $project = Project::factory()->create(['team_id' => $team->id]);
+
+    // Create another user who is not part of the team
+    $unauthorizedUser = User::factory()->create();
+
+    // Check if the unauthorized user can view the project
+    Livewire::actingAs($unauthorizedUser)
+        ->test(ViewProject::class, ['project' => $project])
+        ->assertForbidden();
+
+    // Check if a team member can view the project
+    Livewire::actingAs($teamMember)
+        ->test(ViewProject::class, ['project' => $project])
         ->assertSee($project->title)
         ->assertOk();
-})->todo();
 
-it('can not access another project', function () {
-    $user = User::factory()->create();
-    $project = Project::factory()->create();
-
-    Livewire::actingAs($user)
-        ->test(ViewProject::class)
+    // Check if the team leader can view the project
+    Livewire::actingAs($teamLeader)
+        ->test(ViewProject::class, ['project' => $project])
         ->assertSee($project->title)
         ->assertOk();
 })->todo();
